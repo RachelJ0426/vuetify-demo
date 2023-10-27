23ae10102020f70b7be9ce2935212f8e7cbb5ee4
RachelJ0426 <137344614+RachelJ0426@users.noreply.github.com>
2023-10-27 12:57:34 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# vuetify-loader-issue
+
+1. `npm i`
+2. `npm start`
+3. See the errors:
+    - `modulesTypeError: options.awaitResolve is not a function`
+    - `SassError: This variable was not declared with !default in the @used module.`
---
+++ src/app.vue
@@ -0,0 +1,19 @@
+<template id="App">
+  <v-container>
+    <v-btn>
+      <v-tooltip text="Example" activator="parent" />
+      select
+    </v-btn>
+    <v-list>
+      <v-list-item>...</v-list-item>
+    </v-list>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+  console.log("script setup");
+</script>
+
+<style lang="scss" scoped>
+  //   @use "./settings" as x;
+</style>
